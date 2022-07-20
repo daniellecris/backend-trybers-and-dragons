eddb1b610033c2afefd78d35a8b0cb373703f0cc
Danielle Cristina <dnaielle.trybe@gmail.com>
2022-07-20 12:32:57 -0300

    feat: requisitos 07 e 09

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -1,3 +1,4 @@
+import SimpleFighter from './SimpleFighter';
 import Energy from '../Energy';
 
 interface Fighter {
@@ -5,9 +6,9 @@ interface Fighter {
   strength: number;
   defense: number;
   energy?: Energy;
-  attack(enemy: Fighter): void;
-  special(enemy: Fighter): void;
-  levelUp(): void;
+  attack(enemy: Fighter | SimpleFighter): void;
+  special?(enemy: Fighter | SimpleFighter): void;
+  levelUp:() => void;
   receiveDamage(attackPoints: number): number
 }
 
